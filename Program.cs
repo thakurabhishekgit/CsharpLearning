01d057d2eaf623a7d8819fcc02362aa21719eb22
Singh <Abhishek.Singh@nmrk.com>
2026-05-13 17:22:12 +0530

    C# folder

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+    static void Main()
+    {
+
+        // int val = 0;
+        // int val = int.Parse(Console.ReadLine());
+
+        if (int.TryParse(Console.ReadLine() , out int val))
+        {
+            val += 10;
+            Console.WriteLine(val);
+        } else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+
+        Console.WriteLine(val + 10);
+
+       
+    }
+}
