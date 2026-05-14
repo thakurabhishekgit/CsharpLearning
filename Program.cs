a67835cb1843e364f5d1f7a7f7706efbe46398da
Singh <Abhishek.Singh@nmrk.com>
2026-05-14 12:19:39 +0530

    adding example files

--- Program.cs
+++ Program.cs
@@ -8,31 +8,68 @@
 // │    └── Class1.cs
 // └── ProjectB
 //      └── Class1.cs
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main()
-        {
 
 
-            if (int.TryParse(Console.ReadLine(), out int val))
-            {
-                val += 10;
-                Console.WriteLine(val);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid integer.");
-            }
+//namespace App1
+//{
+//    class Program { }
+//}
 
-            Console.WriteLine(val + 10);
+//namespace App2
+//{
+//    class Program { }
+//}
+
+//namespace HelloWorld
+//{
+//    class Program
+//    {
+//        static void Main()
+//        {
 
 
-        }
-    }
+//            if (int.TryParse(Console.ReadLine(), out int val))
+//            {
+//                val += 10;
+//                Console.WriteLine(val);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid input. Please enter a valid integer.");
+//            }
+
+//            Console.WriteLine(val + 10);
+
+
+//        }
+//    }
     
 
 
 
+//}
+
+
+
+namespace Abhishek
+{
+    class Program
+    {
+        static void intern()
+        {
+            Console.WriteLine("Hello, Abhishek!");
+        }
+    }
+}
+
+
+namespace Kashish
+{
+    class Program
+    {
+        static void intern()
+        {
+            Console.WriteLine("Hello, Kashish!");
+        }
+    }
 }
